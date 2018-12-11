bddb5ea6cf39fc7444e1a5bf4c7a633ba239f02f
medbenchohra <medbenchohra96@gmail.com>
2018-12-11 13:13:49 +0100

    Benchohra : renaming repo from TP3 to TP4

--- graph.py
+++ graph.py
@@ -1,39 +1,41 @@
 import networkx as nx
-import plotly as plt
+import matplotlib.pyplot as plt
 
 
 def createGraph():
    return nx.Graph()
 
-
 def addNode(g,n,visited):
    g.add_node(n, label=visited)
+
 def addEdge(g,nodea,nodeb,weightValue):
-    g.add_edge(nodea,nodeb, weight=weightValue)
+    g.add_edge(nodea, nodeb, weight=weightValue)
+
 def drawGraph(g):
     nx.draw(g)
-    plt
+    plt.show()
+
 def isLinked(g,i,j):
     return(g.get_edge_data(i,j))
+
 g=createGraph()
 addNode(g,1,'false')
 addNode(g,2,'false')
 addNode(g,3,'false')
 addEdge(g,1,2,3)
 addEdge(g,2,3,5)
-drawGraph(g)
 l=[]
 ll=[]
-def dfs(i):
-    setattr(g[i],'label','true')
-    for k in range(g.number_of_nodes()):
-        if((isLinked(g,k,i)) & (g[k]['label']=='false')):
-            l.__add__(g[k])
-            dfs(k)
-            break
-        elif((isLinked(g,k,i))):
+drawGraph(g)
 
-
-
-
-
+# def dfs(i):
+    # setattr(g[i],'label','true')
+    # for k in range(g.number_of_nodes()):
+    #     if((isLinked(g,k,i)) & (g[k]['label']=='false')):
+    #         l.__add__(g[k])
+    #         dfs(k)
+    #         break
+    #     elif((isLinked(g,k,i))):
+    #         break
+	
+def yes(wah) #added to rename the the repo 
